3625f91509344d4b2c1cbb6184b61396b5165640
jiyoung0719 <jiyoungmerong@gamil.com>
2022-09-01 14:07:27 +0900

    백준 25314 코딩은 체욱과목 입니다

---
+++ 220901/baek_25314.java
@@ -0,0 +1,13 @@
+package algorithm;
+
+import java.util.Scanner;
+public class baek_25314 {
+	public static void main(String[] arigs) {
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt()/4;
+        
+		for(int i = 0; i<num; i++) 
+			System.out.print("long ");
+		System.out.print("int");	
+	}
+}
